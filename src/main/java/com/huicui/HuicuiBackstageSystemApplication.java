08c89c7941fc0d8fac3739ff075de818fe4ca09e
hua9575804 <9575804@qq.com>
2019-05-15 15:48:47 +0800

    后台管理系统构建

---
+++ src/main/java/com/huicui/HuicuiBackstageSystemApplication.java
@@ -0,0 +1,13 @@
+package com.huicui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HuicuiBackstageSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HuicuiBackstageSystemApplication.class, args);
+    }
+
+}
